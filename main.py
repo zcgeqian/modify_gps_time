5fce89526a79c6be2f2cac405ed6f3b2193bd76e
chaozhang <zcgeqian@126.com>
2019-03-13 00:21:46 +0800

    add 12h function

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import gpxpy
 import gpxpy.gpx
 from datetime import datetime, timedelta
 
+
 def procXml(gpxPath):
     fileDir, fileName = os.path.split(gpxPath)
-    fileName='update_'+fileName
+    fileName = 'update_'+fileName
     jsonDir = os.path.join(fileDir, 'update')
     if not os.path.exists(jsonDir):
         os.mkdir(jsonDir)
 
     # -----------------------------
-    newGpxPath = os.path.join(jsonDir,fileName)
+    newGpxPath = os.path.join(jsonDir, fileName)
 
     # ----------------------
     # 读取文件
@@ -21,7 +22,7 @@ def procXml(gpxPath):
     t1 = gpx.tracks[0].segments[0].points[0].time
     last_t = t1
     over24Flag = True
-    delta_t=timedelta(seconds=0) #不移动
+    delta_t = timedelta(seconds=0)  # 不移动
     # .total_seconds()
     # 第一次处理，将所有时间变为同一天
     for track in gpx.tracks:
@@ -31,15 +32,26 @@ def procXml(gpxPath):
                 # print(f'当前点时间：{point.time}')
                 cur_t = point.time
                 if (cur_t.day != t1.day and cur_t.day != last_t.day):  # 日期变化,需要向前一定时间
-                    delta_t=delta_t + last_t-cur_t+timedelta(minutes=3) #向前移动的时间段，隔天累加移动时间
+                    delta_t = delta_t + last_t-cur_t + \
+                        timedelta(minutes=3)  # 向前移动的时间段，隔天累加移动时间
 
                 point.adjust_time(delta_t)
-                if (delta_t!=timedelta(seconds=0)):
+                if (delta_t != timedelta(seconds=0)):
                     print(f'修改后时间：{point.time}，时间差：{delta_t}')
                 last_t = cur_t
-                final_t=point.time
-    if (final_t-t1>timedelta(days=1)):
-        print('时间大于24h，对时间进行压缩')
+                final_t = point.time
+
+    if (final_t-t1 > timedelta(hours=12)):
+        print('时间大于12h，对时间进行压缩')
+        totol_t = final_t-t1
+        scale_t = timedelta(hours=11, minutes=55)/totol_t
+
+        for track in gpx.tracks:
+            for segment in track.segments:
+                for point in segment.points:
+                    delta_t = scale_t*(point.time-t1)
+                    print(f'delta_t:{delta_t}')
+                    point.time = t1+delta_t
 
     # print('Created GPX:', gpx.to_xml())
     with open(newGpxPath, 'w', encoding='UTF-8') as f:
