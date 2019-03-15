8dc25fdc42942fac638965fb5ceddf960dc7cfff
chaozhang <zcgeqian@126.com>
2019-03-15 11:22:33 +0800

    modify gpx attr

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ def procXml(gpxPath, time_new):
     # ----------------------
     gpx_file = open(gpxPath, 'r', encoding='UTF-8')
     gpx = gpxpy.parse(gpx_file)
+    gpx.description='朝出品'
+    gpx.keywords='朝出品'
+    gpx.tracks[0].name='朝出品'
     time_s = gpx.tracks[0].segments[0].points[0].time
     last_t = time_s
 
@@ -67,6 +70,7 @@ def procXml(gpxPath, time_new):
         print(f'更新后运动时间为：{cyc_time}')
     
     if time_new is not None:
+        gpx.name=str(time_new.date())+' 骑行'
         time_s=gpx.tracks[0].segments[0].points[0].time
         delta_t=time_new-time_s
         for track in gpx.tracks:
