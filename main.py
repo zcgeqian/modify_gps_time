e36d4b3e4e51c4b3e0e40045bccfe24779d035df
chaozhang <zcgeqian@126.com>
2019-03-15 16:46:14 +0800

    loop for adjust

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ import gpxpy.gpx
 import argparse
 from datetime import datetime, timedelta, timezone
 
+restTime=300
+
+def scale_time(gpx,cyc_time):
+    scale_t = timedelta(hours=11, minutes=30)/cyc_time
+
+    cyc_time = timedelta(seconds=0)
+    time_s=last_t = gpx.tracks[0].segments[0].points[0].time
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                delta_t = scale_t*(point.time-time_s)
+                # print(f'delta_t:{delta_t}')
+                point.time = time_s+delta_t
+                cur_t = point.time
+                if (cur_t-last_t <= timedelta(seconds=restTime)):
+                    cyc_time += (cur_t-last_t)
+                last_t = cur_t
+
+    return gpx,cyc_time
+
 
 def procXml(gpxPath, time_new):
     fileDir, fileName = os.path.split(gpxPath)
@@ -18,6 +38,7 @@ def procXml(gpxPath, time_new):
     # ----------------------
     gpx_file = open(gpxPath, 'r', encoding='UTF-8')
     gpx = gpxpy.parse(gpx_file)
+    gpx_file.close()
     gpx.description='朝出品'
     gpx.keywords='朝出品'
     gpx.tracks[0].name='朝出品'
@@ -30,19 +51,21 @@ def procXml(gpxPath, time_new):
     # 第一次处理,将隔天数据累加起来
     for track in gpx.tracks:
         for segment in track.segments:
-            for point in segment.points:
+            for idp,point in enumerate(segment.points):
                 # print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
                 # print(f'当前点时间：{point.time}')
                 cur_t = point.time
                 if (cur_t.day != time_s.day and cur_t.day != last_t.day):  # 日期变化
                      # 向前移动的时间段，隔天累加移动时间
-                    delta_t = delta_t + last_t-cur_t + timedelta(minutes=3)
+                    delta_t = delta_t + last_t-cur_t + timedelta(seconds=40)
 
                 point.adjust_time(delta_t)
                 # if (delta_t != timedelta(seconds=0)):
                 #     print(f'修改后时间：{point.time}，时间差：{delta_t}')
                 # # compute cyc_time
-                if (cur_t-last_t > timedelta(seconds=60)):
+                # if idp%10==0:
+                # print(f'time is {point.time}')
+                if (cur_t-last_t > timedelta(seconds=restTime)):
                     pass
                 else:
                     cyc_time += (cur_t-last_t)
@@ -50,23 +73,9 @@ def procXml(gpxPath, time_new):
                 last_t = cur_t
 
     print(f'运动时间为：{cyc_time}')
-
-    if (cyc_time > timedelta(hours=12)):
-        print(f'运动时间为{cyc_time},大于12h，对时间进行压缩')
-        scale_t = timedelta(hours=11, minutes=30)/cyc_time
-
-        cyc_time = timedelta(seconds=0)
-        last_t = gpx.tracks[0].segments[0].points[0].time
-        for track in gpx.tracks:
-            for segment in track.segments:
-                for point in segment.points:
-                    delta_t = scale_t*(point.time-time_s)
-                    # print(f'delta_t:{delta_t}')
-                    point.time = time_s+delta_t
-                    cur_t = point.time
-                    if (cur_t-last_t <= timedelta(seconds=60)):
-                        cyc_time += (cur_t-last_t)
-                    last_t = cur_t
+    
+    while (cyc_time > timedelta(hours=12)):
+        gpx,cyc_time=scale_time(gpx,cyc_time)
         print(f'更新后运动时间为：{cyc_time}')
     
     if time_new is not None:
@@ -77,7 +86,7 @@ def procXml(gpxPath, time_new):
             for segment in track.segments:
                 for point in segment.points:
                     point.adjust_time(delta_t)
-                    print(point.time)
+                    # print(point.time)g
 
     # print('Created GPX:', gpx.to_xml())
     with open(newGpxPath, 'w', encoding='UTF-8') as f:
@@ -94,6 +103,8 @@ def main():
 
     if args.d:
         fileList = glob.glob(args.d+'/*.gpx')
+        if len(fileList)<1:
+            print('the gpx file not found, please check!')
 
     if args.f:
         fileList = [args.f]
